ca67ead412da08788d98077854aaba503a6c311e
poliorcetics <user@MBAlexis.local>
2016-02-10 03:49:50 +0100

    Complétion du terrain
    Le terrain est maintenant entièrement formable en prenant en compte
    l’entourage de chaque case en nombre de mines.
    
    Rapport Alexis mis à jour.

--- application/terrain.py
+++ application/terrain.py
@@ -12,18 +12,21 @@ classes:
         - __init__(largeur, hauteur)
         - affiche_terrain()
         - places_mines(nombres_mines)
+        - place_nombre_mines()
 """
 
 
 class Terrain:
-    """ >> Terrain
+    """ >> class Terrain
+
 largeur : int - largeur du terrain
 hauteur : int - hauteur du terrain
 
 - __init__(largeur, hauteur) -> None
 - affiche_terrain() -> None
 - place_mines(nombre_mines=10) -> self.terrain, liste
-    """
+- place_nombre_mines() -> self.terrain, liste
+"""
 
     def __init__(self, largeur, hauteur):
 
@@ -60,15 +63,73 @@ nombre demandé."""
                         
                         # On place la mine au point (x, y) 
                         self.terrain[y][x] = 9
-                        mines += 1
+                        mines += 1 
 
         return self.terrain
 
-# Exemples en console
+    def place_nombre_mines(self):
+        """"Met à jour self.terrain de façon à ce que chaque case n'étant pas
+une mine prenne en compte le nombre de mine autour d'elle."""
+
+        # On parcourt self.terrain
+        for y in range(self.hauteur):
+            for x in range(self.largeur):
+
+                # Si y-1 ou x-1 = -1, on risque d'accéder au mauvais élément de
+                # self.terrain lors de la vérification de l'entourage de la case
+                # donc on le place volontairement en dehors des possibilités
+                # d'accès afin de lever une erreur et d'ajouter un 0 au lieu
+                # d'un potentiel 9 lors de la vérification
+                y_moins = y - 1 if y - 1 >= 0 else self.hauteur + 1
+                x_moins = x - 1 if x - 1 >= 0 else self.largeur + 1
+
+                entourage = [] # L'entourage de la case vérifiée
+
+                # Si la case n'est pas une mine, on procède à la vérification
+                # Pour cela, on vérifie les 8 cases l'entourant et on ajoute
+                # leur contenu à 'entourage' si elles sont accessibles
+                # (pas d'IndexError), sinon 0 (pas une mine)
+                if self.terrain[y][x] < 9:                    
+
+                    try: entourage.append(self.terrain[y_moins][x_moins])
+                    except IndexError: entourage.append(0)
+
+                    try: entourage.append(self.terrain[y_moins][x])
+                    except IndexError: entourage.append(0)
+
+                    try: entourage.append(self.terrain[y_moins][x+1])
+                    except IndexError: entourage.append(0)
+
+                    try: entourage.append(self.terrain[y][x+1])
+                    except IndexError: entourage.append(0)
+
+                    try: entourage.append(self.terrain[y+1][x+1])
+                    except IndexError: entourage.append(0)
+
+                    try: entourage.append(self.terrain[y+1][x])
+                    except IndexError: entourage.append(0)
+
+                    try: entourage.append(self.terrain[y+1][x_moins])
+                    except IndexError: entourage.append(0)
+
+                    try: entourage.append(self.terrain[y][x_moins])
+                    except IndexError: entourage.append(0)
+  
+                    # En comptant le nombre de 9 dans l'entourage, on obtient le
+                    # nombre de mines. On passe cette valeur à la case vérifiée.
+                    self.terrain[y][x] = entourage.count(9)
+
+        return self.terrain
+
+# Exemples en console,
+# toutes les valeurs ici sont des valeurs de test uniquement
 if __name__ == "__main__":
     
-    t = Terrain(6, 6)
+    t = Terrain(12, 12)
     t.affiche_terrain()
     print("----------------")
-    t.place_mines(10)
+    t.place_mines(12*12//3)
+    t.affiche_terrain()
+    print("----------------")
+    t.place_nombre_mines()
     t.affiche_terrain()
--- rapports/rapport-alexis.md
+++ rapports/rapport-alexis.md
@@ -1,3 +1,10 @@
-09/02/2016 - Alexis:
+# Fichier de rapport du travail d'**Alexis**
+
+## 09/02/2016:
 
     Création de **terrain.py** et mise en place du fichier **rapport-alexis.md** ainsi que du **README.md**.
+
+## 10/02/2016:
+
+    MàJ de **terrain.py**:
+        Fonction *place_nombre_mines()* créée, elle permet de modifier *self.terrain* afin de le retourner en ayant placé dans chaque case le nombre de mines adjacentes.
