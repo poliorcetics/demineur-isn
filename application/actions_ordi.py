e4d2b12cc9951fbaedc7c1ac3324e88b31c53cd8
poliorcetics <alexis.bourget@gmail.com>
2016-05-16 22:00:40 +0200

    Simplification de actions_ordi.py

--- application/actions_ordi.py
+++ application/actions_ordi.py
@@ -14,12 +14,12 @@ VARIABLES GLOBALES
 FONCTIONS
 
  - verif_etat_partie(valeur_case: int)                      -> (bool or None),
- - fin_partie(message: str)                                 -> (None),
  - reset(hauteur: int, largeur: int, mines: int)            -> (None).
 """
 
 # l'interface de la fenêtre pour la fin de partie
 import tkinter as tk
+import tkinter.messagebox as mb
 import interface as iu
 
 
@@ -60,41 +60,20 @@ Retourne:
         # Si la case est une mine, on fait perdre le joueur et on révèle le
         # plateau complet (les drapeaux sont remplacés par leur vraie valeur)
         if valeur_case == 9:
-            fin_partie("Vous avez perdu ! :'(")
             fini = True
             iu.lb_chrono.stop_chrono()
             revele_plateau()
+            mb.showinfo("Partie finie !", "Vous avez perdu ! :(")
             return False
 
         # Si le joueur a découvert toutes les cases non-minées, alors on
         # termine aussi la partie en le faisant gagner et on révèle les mines
         if nb_mines + nb_cases_vues == nb_cases:
-            fin_partie("Vous avez gagné ! :D")
             fini = True
             iu.lb_chrono.stop_chrono()
             revele_plateau()
+            mb.showinfo("Partie finie !", "Vous avez gagné ! :D")
             return True
-
-
-def fin_partie(message: str) -> (None):
-    """Affiche le message signalant la fin de partie.
-
-Argument:
- - message      - str - le message de fin de partie."""
-
-    # Une fenêtre pop-up
-    toplevel = tk.Toplevel()
-
-    # Le message de fin de partie
-    lb_message = tk.Label(master=toplevel, text=message)
-    lb_message.pack()
-
-    # Le bouton pour fermer la fenêtre
-    btn_ok = tk.Button(master=toplevel, text='Ok', command=toplevel.destroy)
-    btn_ok.pack()
-
-    # Force la focus sur cette pop-up
-    toplevel.focus_force()
 
 
 def reset(hauteur: int, largeur: int, mines: int) -> (None):
